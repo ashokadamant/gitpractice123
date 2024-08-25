34a038e6021c444aec0dd7833da23a250eaa7b90
TL <TL@gmail.com>
2024-08-25 15:50:26 +0530

    debguggingrestcommit1

---
+++ BasicDebugging/src/com/nt/test/Grouping.java
@@ -0,0 +1,20 @@
+package com.nt.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Grouping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> numbers=new ArrayList<>(Arrays.asList(1,2,3,1,4,2,2,2,2,6,6,6,1,9));
+		Map<Integer, Optional<Integer>> maps = numbers.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.reducing((a,b)->a+b)));
+         System.out.println(maps);
+	}
+
+}
